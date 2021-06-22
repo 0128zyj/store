5ee241b5765858c85812677515df5f16acaca4ad
zyj <957781799@qq.com>
2021-06-22 17:14:48 +0800

    衣服2

---
+++ DAY2.py
@@ -0,0 +1,147 @@
+DAY1 = "1号"
+NAME1 = "羽绒服"
+pricel1 = 253.6
+num1 = 500
+daynum1 = 10
+
+DAY2 = "2号"
+NAME2 = "牛仔裤"
+pricel2 = 86.3
+num2 = 600
+daynum2 = 60
+
+DAY3 = "3号"
+NAME3 = "风衣"
+pricel3 = 96.8
+num3 = 335
+daynum3 = 43
+
+DAY4 = "4号"
+NAME4 = "皮草"
+pricel4 = 135.9
+num4 = 855
+daynum4 = 63
+
+DAY5 = "5号"
+NAME5 = "T恤"
+pricel5 = 65.8
+num5 = 632
+daynum5 = 63
+
+DAY6 = "6号"
+NAME6 = "衬衫"
+pricel6 = 49.3
+num6 = 562
+daynum6 = 120
+
+DAY7 = "7号"
+DAY8 = "8号"
+DAY9 = "9号"
+DAY10 = "10号"
+DAY11 = "11号"
+DAY12 = "12号"
+DAY13 = "13号"
+DAY14 = "14号"
+DAY15 = "15号"
+DAY16 = "16号"
+DAY17 = "17号"
+DAY18 = "18号"
+DAY19 = "19号"
+DAY20 = "20号"
+DAY21 = "21号"
+DAY22 = "22号"
+DAY23 = "23号"
+DAY24 = "24号"
+DAY25 = "25号"
+DAY26 = "26号"
+DAY27 = "27号"
+DAY28 = "28号"
+DAY29 = "29号"
+DAY30 = "30号"
+
+daynum7 = 72
+daynum8 = 69
+daynum9 = 35
+daynum10 = 140
+daynum11 = 90
+daynum12 = 24
+daynum13 = 45
+daynum14 = 25
+daynum15 = 60
+daynum16 = 129
+daynum17 = 10
+daynum18 = 43
+daynum19 = 63
+daynum20 = 60
+daynum21 = 63
+daynum22 = 60
+daynum23 = 58
+daynum24 = 140
+daynum25 = 48
+daynum26 = 43
+daynum27 = 57
+daynum28 = 10
+daynum29 = 63
+daynum30 = 78
+
+
+
+
+
+
+
+
+
+
+print("--------------------------12月份衣服销售数据------------------------------")
+print("日期","\t\t","服装名城","\t\t","价格/件","\t\t","库存数量","\t\t","销售量/每日")
+print(DAY1,"\t\t",NAME1,"\t\t\t",pricel1,"\t\t\t",num1,"\t\t\t",daynum1)
+print(DAY2,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum2)
+print(DAY3,"\t\t",NAME3,"\t\t\t",pricel3,"\t\t\t",num3,"\t\t\t",daynum3)
+print(DAY4,"\t\t",NAME4,"\t\t\t",pricel4,"\t\t\t",num4,"\t\t\t",daynum4)
+print(DAY5,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum5)
+print(DAY6,"\t\t",NAME6,"\t\t\t",pricel6,"\t\t\t",num6,"\t\t\t",daynum6)
+print(DAY7,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum7)
+print(DAY8,"\t\t",NAME1,"\t\t\t",pricel1,"\t\t\t",num1,"\t\t\t",daynum8)
+print(DAY9,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum9)
+print(DAY10,"\t\t",NAME1,"\t\t\t",pricel1,"\t\t\t",num1,"\t\t\t",daynum10)
+print(DAY11,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum11)
+print(DAY12,"\t\t",NAME4,"\t\t\t",pricel4,"\t\t\t",num4,"\t\t\t",daynum12)
+print(DAY13,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum13)
+print(DAY14,"\t\t",NAME3,"\t\t\t",pricel3,"\t\t\t",num3,"\t\t\t",daynum14)
+print(DAY15,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum15)
+print(DAY16,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum16)
+print(DAY17,"\t\t",NAME1,"\t\t\t",pricel1,"\t\t\t",num1,"\t\t\t",daynum17)
+print(DAY18,"\t\t",NAME3,"\t\t\t",pricel3,"\t\t\t",num3,"\t\t\t",daynum18)
+print(DAY19,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum19)
+print(DAY20,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum20)
+print(DAY21,"\t\t",NAME4,"\t\t\t",pricel4,"\t\t\t",num4,"\t\t\t",daynum21)
+print(DAY22,"\t\t",NAME3,"\t\t\t",pricel3,"\t\t\t",num3,"\t\t\t",daynum22)
+print(DAY23,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum23)
+print(DAY24,"\t\t",NAME2,"\t\t\t",pricel2,"\t\t\t",num2,"\t\t\t",daynum24)
+print(DAY25,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum25)
+print(DAY26,"\t\t",NAME3,"\t\t\t",pricel3,"\t\t\t",num3,"\t\t\t",daynum26)
+print(DAY27,"\t\t",NAME4,"\t\t\t",pricel4,"\t\t\t",num4,"\t\t\t",daynum27)
+print(DAY28,"\t\t",NAME1,"\t\t\t",pricel1,"\t\t\t",num1,"\t\t\t",daynum28)
+print(DAY29,"\t\t",NAME5,"\t\t\t",pricel5,"\t\t\t",num5,"\t\t\t",daynum29)
+print(DAY30,"\t\t",NAME3,"\t\t\t",pricel3,"\t\t\t",num3,"\t\t\t",daynum30)
+print("-----------------------------------------------------------------")
+
+
+yrf = pricel1*(daynum1+daynum8+daynum10+daynum17+daynum28)
+pc = pricel4*(daynum4+daynum12+daynum21+daynum27)
+nzk = pricel2*(daynum2+daynum7+daynum9+daynum11+daynum15+daynum20+daynum24)
+fy = pricel3*(daynum3+daynum14+daynum18+daynum22+daynum26+daynum30)
+cs = pricel6*daynum6
+Tx = pricel5*(daynum5+daynum13+daynum16+daynum19+daynum23+daynum25+daynum29)
+
+sum = float('%.2f'%(yrf+pc+nzk+fy+cs+Tx))
+print("12月份销售总金额:", sum)
+
+print("每件衣服的销售占比:")
+print("羽绒服:", f"{yrf/sum*100:.2f}","%")
+print("牛仔裤:", f"{nzk/sum*100:.2f}","%")
+print("风衣:", f"{fy/sum*100:.2f}","%")
+print("皮草:", f"{pc/sum*100:.2f}","%")
+print("T恤:", f"{Tx/sum*100:.2f}","%")
+print("衬衫:", f"{cs/sum*100:.2f}","%")
