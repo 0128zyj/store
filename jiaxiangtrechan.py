ab19e0ee17a2e6b6e225ba31872db8af3966a058
zyj <957781799@qq.com>
2021-06-22 17:48:03 +0800

    水果特产

---
+++ jiaxiangtrechan.py
@@ -0,0 +1,63 @@
+'''
+    商城系统：
+        print()
+    print("想打印的数据！")
+'''
+id1 = 1
+id2 = 2
+id3 = 3
+id4 = 4
+id5 = 5
+name1 = "磨盘柿"
+name2 = "西瓜"
+name3 = "大桃"
+name4 = "草莓"
+name5 = "板栗"
+pricel1 = 15
+pricel2 = 16
+pricel3 = 15
+pricel4 = 30
+pricel5 = 20
+num1 = 150
+num2 = 250
+num3 = 350
+num4 = 250
+num5 = 200
+level1 = "A+"
+level2 = "A"
+level3 = "B+"
+level4 = "A"
+level5 = "A+"
+area1 = "房山"
+area2 = "大兴"
+area3 = "平谷"
+area4 = "昌平"
+area5 = "怀柔"
+
+
+
+print("--------------------------zyj特产商城系统----------------------------")
+print("编号      名称        价格         数量         品质         产地")
+print("-------------------------------------------------------------------")
+print(id1,"\t\t",name1,"\t\t",pricel1,"\t\t",num1,"\t\t",level1,"\t\t",area1)
+print(id2,"\t\t",name2,"\t\t",pricel2,"\t\t",num2,"\t\t",level2,"\t\t\t",area2)
+print(id3,"\t\t",name3,"\t\t",pricel3,"\t\t",num3,"\t\t",level3,"\t\t",area3)
+print(id4,"\t\t",name4,"\t\t",pricel4,"\t\t",num4,"\t\t",level4,"\t\t\t",area4)
+print(id5,"\t\t",name5,"\t\t",pricel5,"\t\t",num5,"\t\t",level5,"\t\t",area5)
+print("-------------------------------------------------------------------")
+print("总金额：￥",(pricel1*num1+pricel2*num2+pricel3*num3+pricel4*num4+pricel5*num5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
